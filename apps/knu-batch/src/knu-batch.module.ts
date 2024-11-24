14d3b3fa91c231cb1f4deab8af6fad63b0ceddf0
Khusan <khusan7700@gmail.com>
2024-11-24 19:40:23 +0900

    feat: configure enc variables

--- apps/knu-batch/src/knu-batch.module.ts
+++ apps/knu-batch/src/knu-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { KnuBatchController } from './knu-batch.controller';
 import { KnuBatchService } from './knu-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [KnuBatchController],
   providers: [KnuBatchService],
 })
