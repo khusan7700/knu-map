58739da273a1e36d5fda88f6d56a216a54deb187
Khusan <khusan7700@gmail.com>
2024-11-26 01:18:25 +0900

    feat: create checkAuthRoles for testing

--- apps/knu-api/src/components/member/member.resolver.ts
+++ apps/knu-api/src/components/member/member.resolver.ts
@@ -2,6 +2,13 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class MemberResolver {
@@ -19,15 +26,47 @@ export class MemberResolver {
 		return this.memberService.login(input);
 	}
 
+	@UseGuards(AuthGuard)
 	@Mutation(() => String)
-	public async updateMember(): Promise<string> {
+	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
 		console.log('Mutation: updateMember');
 		return this.memberService.updateMember();
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => String)
+	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
+		console.log('Mutation: checkAuth');
+		return `Hi ${memberNick}`;
+	}
+
+	@Roles(MemberType.STUDENT, MemberType.TEACHER)
+	@UseGuards(RolesGuard)
+	@Mutation(() => String)
+	public async checkAuthRoles(@AuthMember() AuthMember: Member): Promise<string> {
+		console.log('Query: : checkAuthRoles');
+		return `Hi ${AuthMember.memberNick},you are ${AuthMember.memberType} (memberId: ${AuthMember._id})`;
 	}
 
 	@Query(() => String)
 	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
 		return this.memberService.getMember();
+	}
+
+	// -----------ADMIN------------
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => String)
+	public async getAllMembersByAdmin(@AuthMember() authMember: Member): Promise<string> {
+		console.log('Mutation: getAllMembersByAdmin');
+		return this.memberService.getAllMembersByAdmin();
+	}
+
+	@Query(() => String)
+	public async updateMemberByAdmin(): Promise<string> {
+		console.log('Query: updateMemberByAdmin');
+		return this.memberService.updateMemberByAdmin();
 	}
 }
--- apps/knu-api/src/libs/enums/member.enum.ts
+++ apps/knu-api/src/libs/enums/member.enum.ts
@@ -2,7 +2,7 @@ import { registerEnumType } from '@nestjs/graphql';
 
 export enum MemberType {
 	STUDENT = 'STUDENT',
-	AGENT = 'TEACHER',
+	TEACHER = 'TEACHER',
 	ADMIN = 'ADMIN',
 }
 
