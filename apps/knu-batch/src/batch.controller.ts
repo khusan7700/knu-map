ef21df95c9d7e5c0008fe710bf4c900c41cb7a38
Khusan <khusan7700@gmail.com>
2024-12-03 14:55:56 +0900

    fix: modify job scheduler business logic

--- apps/knu-batch/src/batch.controller.ts
+++ apps/knu-batch/src/batch.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
-import { KnuBatchService } from './batch.service';
+import { Controller, Get, Logger } from '@nestjs/common';
+import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
-export class KnuBatchController {
-	constructor(private readonly knuBatchService: KnuBatchService) {}
+export class BatchController {
+	private logger: Logger = new Logger('BatchController');
 
+	constructor(private readonly batchService: BatchService) {}
+
+	@Timeout(1000)
+	handleTimeout() {
+		this.logger.debug('BATCH SERVER READY!');
+	}
+	//------------------------------BATCH_ROLLBACK------------------------------
+	@Cron('00 00 * * * *', { name: BATCH_ROLLBACK })
+	public async batchRollback() {
+		try {
+			this.logger['context'] = 'BATCH_ROLLBACK';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	//------------------------------BATCH_TOP_PROPERTIES------------------------------
+	@Cron('20 00 * * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchTopProperties() {
+		try {
+			this.logger['context'] = 'BATCH_TOP_PROPERTIES';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	//------------------------------BATCH_TOP_AGENTS------------------------------
+	@Cron('40 00 * * * *', { name: BATCH_TOP_AGENTS })
+	public async batchTopAgents() {
+		try {
+			this.logger['context'] = 'BATCH_TOP_AGENTS';
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+	/*
+	@Interval(1000)
+	handleInterval() {
+		this.logger.debug('INTERVAL TEST');
+	}
+*/
 	@Get()
 	getHello(): string {
-		return this.knuBatchService.getHello();
+		return this.batchService.getHello();
 	}
 }
--- apps/knu-batch/src/batch.module.ts
+++ apps/knu-batch/src/batch.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { KnuBatchController as BatchController } from './batch.controller';
-import { KnuBatchService as BatchService } from './batch.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { BatchController } from './batch.controller';
+import { BatchService } from './batch.service';
+import PropertySchema from 'apps/knu-api/src/schemas/Property.model';
+import MemberSchema from 'apps/knu-api/src/schemas/Member.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot(),
+		DatabaseModule,
+		ScheduleModule.forRoot(),
+		MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]),
+		MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+	],
 	controllers: [BatchController],
 	providers: [BatchService],
 })
